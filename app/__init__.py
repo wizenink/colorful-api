18ed8b7f3dfa7b0ed8eea7a76c28ff70efcfa8a6
David Maseda <david.maseda@hotmail.com>
2020-02-04 18:45:30 +0100

    WIP 2

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask,request,jsonify,send_file,make_response
 from flask_restful import Api,Resource,reqparse
+from flask_cors import CORS
 from app.tensorflow_model import *
 import base64
 from io import StringIO,BytesIO
 from skimage.io import imsave
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 
 kmodel = getModel()
